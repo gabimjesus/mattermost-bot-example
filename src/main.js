a32d00dc82649289e32ceeb54045a8743ce782be
Hugo Dzin <hugodzin@tokenlab.com.br>
2019-06-19 10:04:26 -0300

    main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 // require('./01-hello');
 // require('./02-message');
-require('./03-mattermost-api');
-// require('./04-sorteio-multi');
+// require('./03-mattermost-api');
+require('./04-sorteio-multi');
